146ae6b264258dd8875ffd42a3e61b67f79606f3
Kevin Booms <mrkbooms@gmail.com>
2023-04-15 11:06:41 -0400

    Server class finished

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -5,29 +5,57 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server implements Runnable {
 
-    private ArrayList<ConnectionHandler> connections;
+    private ArrayList<ConnectionHandler> connections; // a list of connected clients
+    private ServerSocket server; // socket representing the server
+    private boolean done; // for checking if the server was shutdown
+    private ExecutorService pool; // this is the Thread-pool everything will run on
+
+    public Server() {
+        connections = new ArrayList<>();
+        done = false;
+    }
     @Override
     public void run() {
         try {
-            ServerSocket server = new ServerSocket(80); // establish a new server and listen on port 80
-            Socket client = server.accept(); // when the server accepts a connection a new client socket is created
-            ConnectionHandler handler = new ConnectionHandler(client);
-            connections.add(handler);
+            while (!done) {
+                server = new ServerSocket(80); // establish a new server and listen on port 80
+                pool = Executors.newCachedThreadPool();
+                Socket client = server.accept(); // when the server accepts a connection a new client socket is created
+                ConnectionHandler handler = new ConnectionHandler(client);
+                connections.add(handler);
+                pool.execute(handler); // this will run the ConnectionHandler run() function;
+            }
         } catch (IOException e) {
-            // TODO: implement a shutdown function
+            shutdown();
         }
 
     }
 
     public void broadcast(String message) {
-        // shout a message to all clients in the list of connections
+        // server needs to broadcast a message to all clients in the list of connections
         for (ConnectionHandler ch : connections) {
             if (ch != null) {
                 ch.sendMessage(message);
             }
+        }
+    }
+
+    public void shutdown() {
+        try {
+            done = true;
+            if (!server.isClosed()) {
+                server.close();
+            }
+        } catch (IOException d) {
+            // the d is for DEAL WITH IT!
+        }
+        for (ConnectionHandler ch : connections) {
+            ch.shutdown();
         }
     }
 
@@ -55,9 +83,33 @@ public class Server implements Runnable {
                 out.println("Please enter a nickname: ");
                 nickname = in.readLine();
                 System.out.println(nickname + " connected");
+                broadcast(nickname + " joined the chat!");
 
+                // use a while loop to listen for messages from the client
+                String message;
+                while ((message = in.readLine()) != null) {
+                    if(message.startsWith("/nick")) {
+
+                        // /nick command will allow user to change nickname
+                        String[] nickChange = message.split(" ", 2);
+                        if (nickChange.length == 2) {
+                            broadcast(nickname + " changed their handle to " + nickChange[1]);
+                            System.out.println(nickname + " changed their handle to " + nickChange[1]);
+                            nickname = nickChange[1];
+                            out.println("Successfully changed handle to " + nickname);
+                        } else {
+                            out.println("No nickname provided!");
+                        }
+
+                    } else if (message.startsWith("/quit")) {
+                        // /quit command will allow user to quit the application
+                        broadcast(nickname + " left the chat!");
+                    } else {
+                        broadcast(nickname + ": " + message);
+                    }
+                }
             } catch (IOException e) {
-                // TODO: handle it, shutdown?
+                shutdown();
             }
         }
 
@@ -65,5 +117,22 @@ public class Server implements Runnable {
         public void sendMessage(String message) {
             out.println(message);
         }
+
+        public void shutdown() {
+            try {
+                in.close();
+                out.close();
+                if (!client.isClosed()) {
+                    client.close();
+                }
+            } catch (IOException d) {
+                // don't worry about it
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run();
     }
 }
